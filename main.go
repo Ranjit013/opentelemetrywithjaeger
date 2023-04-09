72d0630c56081fd41f242dbd0e15eef1f3a41941
Ranjit Gouda <ranjitgouda013@gmail.com>
2023-04-09 12:35:53 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"log"
+	"opentelemetrywithjaeger/config"
+	"time"
+)
+
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Command jaeger is an example program that creates spans
+// and uploads to Jaeger.
+
+// tracerProvider returns an OpenTelemetry TracerProvider configured to use
+// the Jaeger exporter that will send spans to the provided url. The returned
+// TracerProvider will also use a Resource configured with all the information
+// about the application.
+
+func main() {
+	tp, err := config.TracerProvider("http://localhost:14268/api/traces")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Register our TracerProvider as the global so any imported
+	// instrumentation in the future will default to using it.
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Cleanly shutdown and flush telemetry when the application exits.
+	defer func(ctx context.Context) {
+		// Do not make the application hang when it is shutdown.
+		ctx, cancel = context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}(ctx)
+
+	tr := tp.Tracer("component-main")
+
+	ctx, span := tr.Start(ctx, "foo")
+	defer span.End()
+
+	bar(ctx)
+}
+
+func bar(ctx context.Context) {
+	// Use the global TracerProvider.
+	tr := otel.Tracer("component-bar")
+	_, span := tr.Start(ctx, "bar")
+	span.SetAttributes(attribute.Key("testset").String("value"))
+	defer span.End()
+
+	// Do bar...
+}
